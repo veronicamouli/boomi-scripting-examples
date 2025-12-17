006a6bf8b560eb1ae01a789cc223af943095c0b5
Veronica Chandramouli <veronicachandramouli@gmail.com>
2025-12-17 15:11:28 -0600

    Create string-manipulation.js

---
+++ javascript/string-manipulation.js
@@ -0,0 +1,5 @@
+// Trim, uppercase, and concatenate strings
+let firstName = ' John ';
+let lastName = 'Doe ';
+let fullName = (firstName.trim() + ' ' + lastName.trim()).toUpperCase();
+console.log(fullName); // Output: JOHN DOE
