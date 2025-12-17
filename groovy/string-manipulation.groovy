955af15ad272c725c25026900efc269f351db94e
Veronica Chandramouli <veronicachandramouli@gmail.com>
2025-12-17 15:10:11 -0600

    Create string-manipulation.groovy

---
+++ groovy/string-manipulation.groovy
@@ -0,0 +1,5 @@
+// Trim, uppercase, and concatenate strings
+String firstName = ' John '
+String lastName = 'Doe '
+String fullName = (firstName.trim() + ' ' + lastName.trim()).toUpperCase()
+println(fullName) // Output: JOHN DOE
